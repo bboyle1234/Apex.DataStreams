769b829ca90ca4d021234520b7d013750789d8e8
Benjamin Boyle <bboyle1234@gmail.com>
2019-07-14 23:22:12 -0400

    Optimized SucceedFailOperation

--- Apex.DataStreams/Operations/SucceedFailOperation.cs
+++ Apex.DataStreams/Operations/SucceedFailOperation.cs
@@ -10,12 +10,18 @@ namespace Apex.DataStreams.Operations {
         public TimeStamp EnqueuedAt { get; } = TimeStamp.Now;
         readonly TaskCompletionSource<object> TCS = new TaskCompletionSource<object>(TaskContinuationOptions.RunContinuationsAsynchronously);
 
+        /// <summary>
+        /// Gets a task representing the completion of the <see cref="SucceedFailOperation"/>.
+        /// </summary>
         public Task Task => TCS.Task;
+
+        // Reduce allocations by using a pre-allocated static readonly action reference instead of using a lambda in the ContinueWith method call below.
+        static readonly Action<Task> ObserveException = t => _ = t.Exception;
 
         public SucceedFailOperation() {
             /// TODO: Test and see if this is really necessary to prevent unobserved task exceptions,
             /// since to my limited knowledge, the TCS does not involve the TaskScheduler, I think this should not be necessary.
-            TCS.Task.ContinueWith(t => _ = t.Exception, TaskContinuationOptions.OnlyOnFaulted);
+            TCS.Task.ContinueWith(ObserveException, TaskContinuationOptions.OnlyOnFaulted);
         }
 
         public void Succeed() {
