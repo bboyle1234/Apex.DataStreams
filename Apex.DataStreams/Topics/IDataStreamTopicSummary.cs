29b1fb3fb08d3c620dbb6d442cbe927410b99943
Benjamin Boyle <bboyle1234@gmail.com>
2019-07-21 03:25:24 -0400

    Upated documentation

--- Apex.DataStreams/Topics/IDataStreamTopicSummary.cs
+++ Apex.DataStreams/Topics/IDataStreamTopicSummary.cs
@@ -5,11 +5,18 @@ using System.Threading.Tasks;
 
 namespace Apex.DataStreams.Topics {
 
+    /// <summary>
+    /// Every <see cref="IDataStreamTopic"/> has an <see cref="IDataStreamTopicSummary"/>.
+    /// The <see cref="IDataStreamTopicSummary"/> is responsible for bringing newly-connected clients up to date
+    /// with the most recent information, giving them everything they need to know to immediately sync with all
+    /// new <see cref="IDataStreamTopic"/> messages.
+    /// </summary>
     public interface IDataStreamTopicSummary : IDisposable {
 
         /// <summary>
-        /// Called when a new message is being sent for this particular topic.
-        /// Caller topic guarantees not to interleave concurrent calls to any method in this class.
+        /// Called by the <see cref="IDataStreamTopic"/> when a new message is being sent for this particular topic.
+        /// The caller topic guarantees not to interleave concurrent calls to any method in this class.
+        /// Use this method to implement a state-tracking system that can provide the topic's current state to any new client when it connects.
         /// </summary>
         Task OnMessage(MessageEnvelope envelope);
 
@@ -19,6 +26,11 @@ namespace Apex.DataStreams.Topics {
         /// You can return null to indicate no summary messages need to be sent. 
         /// You can return an array that has null items if not all placeholders in the array have a message to be sent.
         /// </summary>
+        /// <returns>
+        /// An array of objects to be sent, in order, to the newly-connected client.
+        /// You can return null to indicate no messages need to be sent.
+        /// If the returned array contains null values, they will be ignored.
+        /// </returns>
         Task<object[]> GetTopicSummary();
     }
 }
